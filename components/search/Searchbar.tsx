6929c4870637d8c916c3c9ee1e805bb54502858d
Peter Onyegbule <49274497+webstellar@users.noreply.github.com>
2023-11-27 13:56:31 +0100

    Update Searchbar.tsx

--- components/search/Searchbar.tsx
+++ components/search/Searchbar.tsx
@@ -96,97 +96,99 @@ const Searchbar = () => {
           </div>
         </form>
 
-        <div className="flex flex-col justify-center items-center">
-          <div className="relative flex max-w-[500px] h-[430px] w-full flex-col rounded-[10px] border-[1px] border-gray-200 bg-gray-800 bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
-            <div className="flex h-fit w-full items-center justify-between rounded-t-2xl bg-gray-800 px-4 pb-[20px] pt-4 shadow-2xl shadow-gray-100 dark:!bg-navy-700 dark:shadow-none">
-              <h4 className="text-lg font-bold text-navy-700 dark:text-white">
-                Domain Info
-              </h4>
-              <button className="linear rounded-[20px] bg-lightPrimary px-4 py-2 text-base font-medium text-brand-500 transition duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 dark:active:bg-white/20">
-                See more
-              </button>
-            </div>
-            <div className="w-full overflow-x-scroll px-4 md:overflow-x-hidden">
-              <table
-                role="table"
-                className="w-full min-w-[500px] overflow-x-scroll"
-              >
-                <thead>
-                  <tr role="row">
-                    <th
-                      colSpan={1}
-                      role="columnheader"
-                      title="Toggle SortBy"
-                      style={{ cursor: "pointer" }}
-                    >
-                      <div className="flex items-center justify-between pb-2 pt-4 text-start uppercase tracking-wide text-gray-600 sm:text-xs lg:text-xs">
-                        Owner
-                      </div>
-                    </th>
-                    <th
-                      colSpan={1}
-                      role="columnheader"
-                      title="Toggle SortBy"
-                      style={{ cursor: "pointer" }}
-                    >
-                      <div className="flex items-center justify-between pb-2 pt-4 text-start uppercase tracking-wide text-gray-600 sm:text-xs lg:text-xs">
-                        Domain (.sol)
-                      </div>
-                    </th>
-                    <th
-                      colSpan={1}
-                      role="columnheader"
-                      title="Toggle SortBy"
-                      style={{ cursor: "pointer" }}
-                    >
-                      <div className="flex items-center justify-between pb-2 pt-4 text-start uppercase tracking-wide text-gray-600 sm:text-xs lg:text-xs">
-                        Value
-                      </div>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody role="rowgroup" className="px-4">
-                  <tr role="row">
-                    <td
-                      className="py-3 pr-4 text-sm break-all align-top"
-                      role="cell"
-                      width="50%"
-                    >
-                      <div className="flex items-center gap-2">
-                        <p className="text-sm font-medium text-navy-700 dark:text-white">
-                          {result}
-                        </p>
-                      </div>
-                    </td>
-                    <td
-                      className="py-3 pr-4 text-sm break-all align-top"
-                      role="cell"
-                      width="30%"
-                    >
-                      <p className="text-md font-medium text-gray-600 dark:text-white">
-                        {result && domain}
-                      </p>
-                    </td>
-                    <td
-                      className="py-3 pr-2 text-sm align-top"
-                      role="cell"
-                      width="20%"
-                    >
-                      <div className="mx-2 flex font-bold">
-                        <div
-                          className="flex h-full items-center justify-center rounded-md bg-brand-500 dark:bg-brand-400"
-                          style={{ width: "30%" }}
-                        >
-                          $400
+        {result && (
+          <div className="flex flex-col justify-center items-center">
+            <div className="relative flex max-w-[500px] h-[430px] w-full flex-col rounded-[10px] border-[1px] border-gray-200 bg-gray-800 bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
+              <div className="flex h-fit w-full items-center justify-between rounded-t-2xl bg-gray-800 px-4 pb-[20px] pt-4 shadow-2xl shadow-gray-100 dark:!bg-navy-700 dark:shadow-none">
+                <h4 className="text-lg font-bold text-navy-700 dark:text-white">
+                  Domain Info
+                </h4>
+                <button className="linear rounded-[20px] bg-lightPrimary px-4 py-2 text-base font-medium text-brand-500 transition duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 dark:active:bg-white/20">
+                  See more
+                </button>
+              </div>
+              <div className="w-full overflow-x-scroll px-4 md:overflow-x-hidden">
+                <table
+                  role="table"
+                  className="w-full min-w-[500px] overflow-x-scroll"
+                >
+                  <thead>
+                    <tr role="row">
+                      <th
+                        colSpan={1}
+                        role="columnheader"
+                        title="Toggle SortBy"
+                        style={{ cursor: "pointer" }}
+                      >
+                        <div className="flex items-center justify-between pb-2 pt-4 text-start uppercase tracking-wide text-gray-600 sm:text-xs lg:text-xs">
+                          Owner
                         </div>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+                      </th>
+                      <th
+                        colSpan={1}
+                        role="columnheader"
+                        title="Toggle SortBy"
+                        style={{ cursor: "pointer" }}
+                      >
+                        <div className="flex items-center justify-between pb-2 pt-4 text-start uppercase tracking-wide text-gray-600 sm:text-xs lg:text-xs">
+                          Domain (.sol)
+                        </div>
+                      </th>
+                      <th
+                        colSpan={1}
+                        role="columnheader"
+                        title="Toggle SortBy"
+                        style={{ cursor: "pointer" }}
+                      >
+                        <div className="flex items-center justify-between pb-2 pt-4 text-start uppercase tracking-wide text-gray-600 sm:text-xs lg:text-xs">
+                          Value
+                        </div>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody role="rowgroup" className="px-4">
+                    <tr role="row">
+                      <td
+                        className="py-3 pr-4 text-sm break-all align-top"
+                        role="cell"
+                        width="50%"
+                      >
+                        <div className="flex items-center gap-2">
+                          <p className="text-sm font-medium text-navy-700 dark:text-white">
+                            {result}
+                          </p>
+                        </div>
+                      </td>
+                      <td
+                        className="py-3 pr-4 text-sm break-all align-top"
+                        role="cell"
+                        width="30%"
+                      >
+                        <p className="text-md font-medium text-gray-600 dark:text-white">
+                          {result && domain}
+                        </p>
+                      </td>
+                      <td
+                        className="py-3 pr-2 text-sm align-top"
+                        role="cell"
+                        width="20%"
+                      >
+                        <div className="mx-2 flex font-bold">
+                          <div
+                            className="flex h-full items-center justify-center rounded-md bg-brand-500 dark:bg-brand-400"
+                            style={{ width: "30%" }}
+                          >
+                            {result && $400}
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
