30ceb0545a8920004044cb022b0ba8f3d3f91377
Peter Onyegbule <49274497+webstellar@users.noreply.github.com>
2024-03-18 09:31:19 +0100

    Update Searchbar.tsx

--- components/search/Searchbar.tsx
+++ components/search/Searchbar.tsx
@@ -64,7 +64,7 @@ const Searchbar = () => {
               className="w-full md:w-[500px] backdrop-blur-sm bg-white/20 py-2 pl-10 pr-4 rounded-lg focus:outline-none border-2 border-gray-100 focus:border-violet-300 transition-colors duration-300"
               placeholder="Enter a domain name"
             />
-            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center cursor-pointer">
               <svg
                 className="w-4 h-4 text-gray-800 dark:text-black"
                 aria-hidden="true"
